9ad034421e9cfe24c6eff71eaf778d06b4d7dc9c
himanshu.tomar <tomarhimanshu98@gmail.com>
2020-07-17 22:19:13 +0530

    get solution api added

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import request
 import json
 from app import app
 
-from sympy import symbols ,Function, simplify, factor,solve
+from sympy import symbols, Function, simplify, factor, solve
 x, y, z, t = symbols('x y z t')
 k, m, n = symbols('k m n', integer=True)
 f, g, h = symbols('f g h', cls=Function)
@@ -10,11 +10,24 @@ f, g, h = symbols('f g h', cls=Function)
 
 @app.route('/')
 def hello():
-    expr=2*x**2+3*x+1
-    obj={
-        "simplify":str(simplify(expr)),
-        "factor":str(factor(expr)),
-        "solve":str(solve(expr))
+    expr = 2*x**2+3*x+1
+    obj = {
+        "simplify": str(simplify(expr)),
+        "factor": str(factor(expr)),
+        "solve": str(solve(expr))
     }
-    print(obj["factor"])
+    # print(obj["factor"])
     return json.dumps(obj)
+
+
+@app.route('/getSolution', methods=['GET'])
+def getSolution():
+    data = request.get_json()
+    expr=data["expr"]
+    obj = {
+        "simplify": str(simplify(expr)),
+        "factor": str(factor(expr)),
+        "solve": str(solve(expr))
+    }
+    # print(obj["factor"])
+    return json.dumps(obj),200;
