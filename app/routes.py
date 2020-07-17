4d726a51501469823a0c90f8cd92032a1e3d6875
himanshu.tomar <tomarhimanshu98@gmail.com>
2020-07-17 18:43:06 +0530

    sympy library added

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
 from flask import request
-
+import json
 from app import app
- 
+
+from sympy import symbols ,Function, simplify, factor,solve
+x, y, z, t = symbols('x y z t')
+k, m, n = symbols('k m n', integer=True)
+f, g, h = symbols('f g h', cls=Function)
+
+
 @app.route('/')
 def hello():
-    return 'Hello World'
+    expr=2*x**2+3*x+1
+    obj={
+        "simplify":str(simplify(expr)),
+        "factor":str(factor(expr)),
+        "solve":str(solve(expr))
+    }
+    print(obj["factor"])
+    return json.dumps(obj)
